da241dfdc06c28b303ce1dc7ff18270951fe343d
Mikhail Temkine <miktemk@gmail.com>
2018-05-10 21:37:24 -0400

    initial

---
+++ AvalonEditSyntaxHighlightEditor/Code/MyIdeUtils.cs
@@ -0,0 +1,34 @@
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AvalonEditSyntaxHighlightEditor.Code
+{
+    public class MyIdeUtils
+    {
+        public static IHighlightingDefinition LoadSyntaxHighlightingFromResource(string resourceName)
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            using (XmlTextReader xshd_reader = new XmlTextReader(stream))
+            {
+                return HighlightingLoader.Load(xshd_reader, HighlightingManager.Instance);
+            }
+        }
+
+        public static IHighlightingDefinition LoadSyntaxHighlightingFromString(string xshdScript)
+        {
+            using (var reader = new StringReader(xshdScript))
+            using (XmlTextReader xshd_reader = new XmlTextReader(reader))
+            {
+                return HighlightingLoader.Load(xshd_reader, HighlightingManager.Instance);
+            }
+        }
+    }
+}
---
+++ AvalonEditSyntaxHighlightEditor/ViewModel/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using AvalonEditSyntaxHighlightEditor.Code;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+using ICSharpCode.AvalonEdit.Highlighting;
+using System.Windows.Input;
+using System;
+using System.Diagnostics;
+
+namespace AvalonEditSyntaxHighlightEditor.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string WelcomeTitle { get; set; } = "MVVM test app";
+
+        // avalon-edit
+        public TextDocument CodeDocument { get; } = new TextDocument();
+        public TextDocument CodeDocumentSample { get; } = new TextDocument();
+        public IHighlightingDefinition SyntaxHighlightingSample { get; set; }
+
+        // commands
+        public ICommand WindowLoadedCommand { get; }
+        public ICommand WindowClosingCommand { get; }
+        public ICommand TriggerBuildCommand { get; }
+        public ICommand CaretPositionChangedCommand { get; }
+
+        public MainViewModel()
+        {
+            // set up view
+            CodeDocument.Text = @"";
+            CodeDocumentSample.Text = @"";
+
+            // assign commands
+            WindowLoadedCommand = new RelayCommand(WindowLoaded);
+            WindowClosingCommand = new RelayCommand(WindowClosing);
+            TriggerBuildCommand = new RelayCommand(TriggerBuild);
+            CaretPositionChangedCommand = new RelayCommand<Caret>(CaretPositionChanged);
+        }
+
+        #region ------------------ commands -----------------------
+
+        private void WindowLoaded() { }
+
+        private void WindowClosing() { }
+
+        private void TriggerBuild()
+        {
+            try
+            {
+                SyntaxHighlightingSample = MyIdeUtils.LoadSyntaxHighlightingFromString(CodeDocument.Text);
+            }
+            catch (ICSharpCode.AvalonEdit.Highlighting.HighlightingDefinitionInvalidException ex)
+            {
+                
+            }
+        }
+
+        private void CaretPositionChanged(Caret caret)
+        {
+            var curCaretOffset = caret.Offset;
+            var curCaretLine = CodeDocument.GetLineByNumber(caret.Line);
+            var curLineText = CodeDocument.GetText(curCaretLine);
+        }
+
+        #endregion
+    }
+}
